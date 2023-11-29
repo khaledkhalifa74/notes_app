49ec9a0fd32a2c098ad085eb8d473f2b7c12bc0b
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2023-11-29 18:11:11 +0200

    create validator

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  const CustomButton({super.key, required this.onPressed});
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(Colors.tealAccent),
         overlayColor: MaterialStateProperty.all(Colors.teal),
